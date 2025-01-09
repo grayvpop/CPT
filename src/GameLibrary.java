52549bb30654905cef4120d3d2a3d20a0ffb4ed2
Nate <killavilla05@gmail.com>
2025-01-09 08:13:19 -0800

    created a folder where the inputted game are going to go and created a gameslibrary file where the main code is going to be written

---
+++ src/GameLibrary.java
@@ -0,0 +1,5 @@
+public class GameLibrary {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Welcome to the library of games! Enjoy your stay =)");
+    }
+}
